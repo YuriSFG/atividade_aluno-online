6b7d9d703f15747c43c3e6594fb15bace1c23c7d
YuriSFG <yurisfg@gmail.com>
2023-06-14 21:35:14 -0300

    finalizado arquivo teste do cypress!

--- test/unit/specs/cy.test.js
+++ test/unit/specs/cy.test.js
@@ -1,33 +1,58 @@
-describe('Login', () => {
-    it('Realiza o login com sucesso', () => {
-    
-      cy.visit('http://localhost:3000/login');
-  
-      
-      cy.get('input[name="username"]').type('2023100');
-  
-      
-      cy.get('input[name="password"]').type('senha123');
-  
-      
-      cy.get('.btn').click();
-  
-      
-      cy.url().should('eq', 'http://localhost:3000/');
-      cy.contains('Olá, Edgar Amaya!').should('be.visible');
-    });
-  
-    it('Senha inválida!', () => {
-      cy.visit('http://localhost:3000/login');
-  
-      
-      cy.get('input[alunoline]').type('usuario123');
-      cy.get('input[senha321]').type('senhaerrada');
-  
-      cy.get('.btn]').click();
-  
-      
-      cy.contains('Credenciais inválidas').should('be.visible');
-    });
+describe('Testes do conceito médio CA', () => {
+  it('retorna conceito SS - Superior', () => {
+    cy.visit('/notas');
+    cy.url().should('eq', 'http://localhost:3000/notas');
+    cy.get('#nota-input').type('10');
+    cy.get('#calcular-button').click();
+    cy.get('#conceito').should('contain', 'SS - Superior');
   });
-  
+
+  it('retorna conceito MS - Médio Superior', () => {
+    cy.visit('/notas');
+    cy.url().should('eq', 'http://localhost:3000/notas');
+    cy.get('#nota-input').type('8.0');
+    cy.get('#calcular-button').click();
+    cy.get('#conceito').should('contain', 'MS - Médio Superior');
+  });
+
+  it('retorna conceito MM - Médio', () => {
+    cy.visit('/notas');
+    cy.url().should('eq', 'http://localhost:3000/notas');
+    cy.get('#nota-input').type('6.5');
+    cy.get('#calcular-button').click();
+    cy.get('#conceito').should('contain', 'MM - Médio');
+  });
+
+  it('retorna conceito MI - Médio Inferior', () => {
+    cy.visit('/notas');
+    cy.url().should('eq', 'http://localhost:3000/notas');
+    cy.get('#nota-input').type('2.2');
+    cy.get('#calcular-button').click();
+    cy.get('#conceito').should('contain', 'MI - Médio Inferior');
+  });
+
+  it('retorna conceito II - Inferior', () => {
+    cy.visit('/notas');
+    cy.url().should('eq', 'http://localhost:3000/notas');
+    cy.get('#nota-input').type('1.1');
+    cy.get('#calcular-button').click();
+    cy.get('#conceito').should('contain', 'II - Inferior');
+  });
+
+  it('retorna conceito SR - Sem rendimento', () => {
+    cy.visit('/notas');
+    cy.url().should('eq', 'http://localhost:3000/notas');
+    cy.get('#nota-input').type('0.0');
+    cy.get('#calcular-button').click();
+    cy.get('#conceito').should('contain', 'SR - Sem rendimento');
+  });
+
+  it('exibe mensagem de erro para nota inválida', () => {
+    cy.visit('/notas');
+    cy.url().should('eq', 'http://localhost:3000/notas');
+    cy.get('#nota-input').type('-1');
+    cy.get('#calcular-button').click();
+    cy.get('#conceito').should('not.exist');
+    cy.get('.error-message').should('exist');
+  });
+});
