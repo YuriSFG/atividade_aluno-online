8b1601587e8df2bc51f0d40351829558b7c936d2
YuriSFG <yurisfg@gmail.com>
2023-06-14 21:51:12 -0300

    Correção do arquivo nota.js

--- test/unit/specs/nota.test.js
+++ test/unit/specs/nota.test.js
@@ -1,58 +1,33 @@
-describe('Testes do conceito médio CA', () => {
-    it('retorna conceito SS - Superior', () => {
-        cy.visit('/notas');
-        cy.url().should('eq', 'http://localhost:3000/notas');
-        cy.get('#nota-input').type('9');
-        cy.get('#calcular-button').click();
-        cy.get('#conceito').should('contain', 'SS - Superior');
+const Nota = require('../src/models/nota');
+
+describe('Cálculo da média final e retorno do conceito', () => {
+    test('retorna o conceito SS - Superior para média >= 9,0', () => {
+        let nota = new Nota(null, 10, 10, 10);
+        expect(nota.MediaFinal()).toEqual('SS - Superior');
     });
 
-    it('retorna conceito MS - Médio Superior', () => {
-        cy.visit('/notas');
-        cy.url().should('eq', 'http://localhost:3000/notas');
-        cy.get('#nota-input').type('7.2');
-        cy.get('#calcular-button').click();
-        cy.get('#conceito').should('contain', 'MS - Médio Superior');
+    test('retorna o conceito MS - Médio Superior para média entre 7,0 e 8,9', () => {
+        let nota = new Nota(null, 7.5, 8, 6.5);
+        expect(nota.MediaFinal()).toEqual('MS - Médio Superior');
     });
 
-    it('retorna conceito MM - Médio', () => {
-        cy.visit('/notas');
-        cy.url().should('eq', 'http://localhost:3000/notas');
-        cy.get('#nota-input').type('6.6');
-        cy.get('#calcular-button').click();
-        cy.get('#conceito').should('contain', 'MM - Médio');
+    test('retorna o conceito MM - Médio para média entre 5,0 e 6,9', () => {
+        let nota = new Nota(null, 5, 5.5, 6);
+        expect(nota.MediaFinal()).toEqual('MM - Médio');
     });
 
-    it('retorna conceito MI - Médio Inferior', () => {
-        cy.visit('/notas');
-        cy.url().should('eq', 'http://localhost:3000/notas');
-        cy.get('#nota-input').type('2.5');
-        cy.get('#calcular-button').click();
-        cy.get('#conceito').should('contain', 'MI - Médio Inferior');
+    test('retorna o conceito MI - Médio Inferior para média entre 3,0 e 4,9', () => {
+        let nota = new Nota(null, 3.5, 4, 3);
+        expect(nota.MediaFinal()).toEqual('MI - Médio Inferior');
     });
 
-    it('retorna conceito II - Inferior', () => {
-        cy.visit('/notas');
-        cy.url().should('eq', 'http://localhost:3000/notas');
-        cy.get('#nota-input').type('0.5');
-        cy.get('#calcular-button').click();
-        cy.get('#conceito').should('contain', 'II - Inferior');
+    test('retorna o conceito II - Inferior para média entre 0,1 e 2,9', () => {
+        let nota = new Nota(null, 1, 2, 1.5);
+        expect(nota.MediaFinal()).toEqual('II - Inferior');
     });
 
-    it('retorna conceito SR - Sem rendimento', () => {
-        cy.visit('/notas');
-        cy.url().should('eq', 'http://localhost:3000/notas');
-        cy.get('#nota-input').type('0');
-        cy.get('#calcular-button').click();
-        cy.get('#conceito').should('contain', 'SR - Sem rendimento');
-    });
-
-    it('exibe mensagem de erro para nota inválida', () => {
-        cy.visit('/notas');
-        cy.url().should('eq', 'http://localhost:3000/notas');
-        cy.get('#nota-input').type('-2');
-        cy.get('#calcular-button').click();
-        cy.get('#conceito').should('not.exist');
-        cy.get('.error-message').should('exist');
+    test('retorna o conceito SR - Sem rendimento para média igual a 0,0', () => {
+        let nota = new Nota(null, 0, 0, 0);
+        expect(nota.MediaFinal()).toEqual('SR - Sem rendimento');
     });
 });
