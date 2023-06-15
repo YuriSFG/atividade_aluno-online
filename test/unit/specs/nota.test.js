70eeb201b0a33756fdc936eb72588e5eb9895122
YuriSFG <yurisfg@gmail.com>
2023-06-14 21:39:48 -0300

    Atualização do arquivo de teste notas!

--- test/unit/specs/nota.test.js
+++ test/unit/specs/nota.test.js
@@ -1,35 +1,51 @@
-const Nota = require('../src/models/nota');
-
-describe('Calculo da média final', () => {
-    test('a media é zero se não tem notas', () => {
-        let nota = new Nota(null, 0, 0, 0);
-        expect(nota.mediaFinal()).toEqual(0);
-        nota = new Nota(null, null, null, null)
-        expect(nota.mediaFinal()).toEqual(0);
+describe('Testes do conceito médio CA', () => {
+    it('retorna conceito SS - Superior', () => {
+      cy.visit('/');
+      cy.get('#nota-input').type('9.5');
+      cy.get('#calcular-button').click();
+      cy.get('#conceito').should('contain', 'SS - Superior');
     });
 
-    test('a média é (0.4 * a1) + (0.6 * a2) se tem apenas a1 e a2', () => {
-        let nota = new Nota(null, 3, 5, null);
-        expect(nota.mediaFinal()).toEqual(0.4 * 3 + 0.6 * 5);
-        nota = new Nota(null, 7, 4, null);
-        expect(nota.mediaFinal()).toEqual(0.4 * 7 + 0.6 * 4);
+    it('retorna conceito MS - Médio Superior', () => {
+      cy.visit('/');
+      cy.get('#nota-input').type('7.5');
+      cy.get('#calcular-button').click();
+      cy.get('#conceito').should('contain', 'MS - Médio Superior');
     });
 
-    test('a média é (0.4 * a3) + (0.6 * a2) se a3 substitui a1', () => {
-        let nota = new Nota(null, 0, 5, 3);
-        expect(nota.mediaFinal()).toEqual(0.4 * 3 + 0.6 * 5);
-        nota = new Nota(null, 2, 5, 3);
-        expect(nota.mediaFinal()).toEqual(0.4 * 3 + 0.6 * 5);
-        nota = new Nota(null, 2, 5, 6);
-        expect(nota.mediaFinal()).toEqual(0.4 * 6 + 0.6 * 5);
+    it('retorna conceito MM - Médio', () => {
+      cy.visit('/');
+      cy.get('#nota-input').type('5.5');
+      cy.get('#calcular-button').click();
+      cy.get('#conceito').should('contain', 'MM - Médio');
     });
 
-    test('a média é (0.4 * a1) + (0.6 * a3) se a3 substitui a2', () => {
-        let nota = new Nota(null, 6, 0, 5);
-        expect(nota.mediaFinal()).toEqual(0.4 * 6 + 0.6 * 5);
-        nota = new Nota(null, 6, 4, 5);
-        expect(nota.mediaFinal()).toEqual(0.4 * 6 + 0.6 * 5);
-        nota = new Nota(null, 6, 4, 7);
-        expect(nota.mediaFinal()).toEqual(0.4 * 6 + 0.6 * 7);
+    it('retorna conceito MI - Médio Inferior', () => {
+      cy.visit('/');
+      cy.get('#nota-input').type('3.5');
+      cy.get('#calcular-button').click();
+      cy.get('#conceito').should('contain', 'MI - Médio Inferior');
     });
-});
+
+    it('retorna conceito II - Inferior', () => {
+      cy.visit('/');
+      cy.get('#nota-input').type('1.5');
+      cy.get('#calcular-button').click();
+      cy.get('#conceito').should('contain', 'II - Inferior');
+    });
+
+    it('retorna conceito SR - Sem rendimento', () => {
+      cy.visit('/');
+      cy.get('#nota-input').type('0.0');
+      cy.get('#calcular-button').click();
+      cy.get('#conceito').should('contain', 'SR - Sem rendimento');
+    });
+
+    it('exibe mensagem de erro para nota inválida', () => {
+      cy.visit('/');
+      cy.get('#nota-input').type('-1');
+      cy.get('#calcular-button').click();
+      cy.get('#conceito').should('not.exist');
+      cy.get('.error-message').should('exist');
+    });
+  });
